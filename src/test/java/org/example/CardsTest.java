335cc780052dd4a335d1d34818bbeff8f56bc741
Szymon Szczubkowski <szyszc4@st.amu.edu.pl>
2024-01-20 23:38:04 +0100

    add: test with parameters update: names of tests

--- src/test/java/org/example/CardsTest.java
+++ src/test/java/org/example/CardsTest.java
@@ -23,12 +23,20 @@ public class CardsTest {
     }
 
     @Test
-    public void getCardsWithInvalidIDShouldReturnErrorNotFound(){
+    public void getCardsWithInvalidPathShouldReturnErrorNotFound(){
         given().pathParam("id", 0).get("/{id}").then().statusCode(404);
     }
 
     @Test
-    public void getCardsWithSpecificIDShouldReturnCorrectData(){
+    public void getCardsWithParameterShouldReturnCorrectData(){
+        given().
+                param("id", "33b9ca30-0296-52b7-a8e2-7d4715404b0d|f592d20b-1974-569e-82ab-aa59f3a66765").
+        get().then().
+                header("Count", equalTo("2"));
+    }
+
+    @Test
+    public void getCardsWithSpecificPathShouldReturnCorrectData(){
         Map<Integer, String> expectedNames = Map.of(
                 1, "Ankh of Mishra",
                 2, "Basalt Monolith",
